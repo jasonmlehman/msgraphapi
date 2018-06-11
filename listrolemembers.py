1f0c4f4c50e20139a30b1a69ff42df330b11e7ca
Lehman, Jason M <jason.lehman@spectrumhealth.org>
2018-06-11 09:17:16 -0400

    Adding script to Query Office 365 Roles for members

---
+++ listrolemembers.py
@@ -0,0 +1,46 @@
+from msgraphapi import msgraphapi
+import argparse
+import sys
+
+# Lists all the members of an Office 365 Role
+# Usage example: python listrolemembers.py -role "Company administrator" -environment test
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-role', '--role',
+                    help='The Office 365 Role to query')
+parser.add_argument('-environment', '--environment',
+                    help='select environment: test/prod')
+parser.add_argument('-listrole', '--listrole',
+                    help='If argument is set will list all available office 365 roles')
+
+args = parser.parse_args()
+role = args.role
+env = args.environment
+rolelist = args.listrole
+
+if rolelist:
+        r = msgraphapi(environment=env)
+        result = r.listroles()
+        for roles in result:
+                print roles
+        sys.exit(1)
+
+if role == None:
+        print("Please specify role using -role parameter")
+        sys.exit(1)
+
+if env == None:
+        print("Please specify environment (test/prod) using -environment parameter")
+        sys.exit(1)
+
+r = msgraphapi(environment=env)
+
+# Need to get the ID of the directory object for the role
+
+roleid = r.getroleid(role)
+
+#  Now that we have directory objects we can make the request
+
+result = r.listrolemembers(roleid)
+for roles in result:
+        print roles
